7f346e72db3b73f2d09d67f82a67de04b97ad249
Clint Grounds <clintgrounds@gmail.com>
2016-10-05 08:59:34 -0500

    ATM Homework Complete

--- starter/js/atm.js
+++ starter/js/atm.js
@@ -1,15 +1,42 @@
+console.log("clint");
+$(document).ready(function () {
+  $('#depositChecking').on('click', checkingAccountDepost)
+  $('#withdrawChecking').on('click', checkingAccountWithdrawl)
+  $('#depositSavings').on('click', savingsAccountDeposit)
+  $('#withdrawSavings').on('click', savingsAccountWithdrawl)
+  $('#withdrawChecking').on('click', overdraftProtection)
+});
+
+  $('body').css('background-color','red');
 //Begin with the document ready function
 
     //Checking account deposit function
+      // var checkingInput = $('#amountChecking').val()
 
-      //On click of the depositChecking button
+      // var checkingWithdawBalance = checkingInput -= checkingBalance
 
-        //Get value from the amountChecking input field
 
-        //Take that value and add it to the existing value in the checkingBalance div
+    function checkingAccountDepost() {
+      var checkingBal = $('#checkingBalance').text().replace('$','');
+      var checkingNum = parseInt(checkingBal);
+      var checkingInput = $('#amountChecking').val()
+      var checkingNumTwo = parseInt(checkingInput);
+      checkingBal = checkingNum+checkingNumTwo;
+      $('#checkingBalance').text('$'+checkingBal)
+    };
 
     //Checking account withdrawl funtion
-
+    function checkingAccountWithdrawl() {
+      var checkingBal = $('#checkingBalance').text().replace('$','');
+      var checkingNum = parseInt(checkingBal);
+      var checkingInput = $('#amountChecking').val()
+      var checkingNumTwo = parseInt(checkingInput);
+      checkingBal = checkingNum-checkingNumTwo;
+      // if (checkingBal<0) {
+      //   return alert('Your account is too low!')
+      // }
+      // $('#checkingBalance').text('$'+checkingBal)
+    }
       //On click of the withdrawChecking button
 
         //Get value from the amountChecking input field
@@ -20,7 +47,14 @@
         //Else subtract that value from the current amount in the checking account
 
     //Savings account deposit function
-
+    function savingsAccountDeposit() {
+      var savingsBal = $('#savingsBalance').text().replace('$','');
+      var savingsNum = parseInt(savingsBal);
+      var savingsInput = $('#amountSavings').val()
+      var savingsNumTwo = parseInt(savingsInput);
+      savingsBal = savingsNum+savingsNumTwo;
+      $('#savingsBalance').text('$'+savingsBal)
+    };
       //On click of the depositSavings button
 
         //Get value from the amountSavings input field
@@ -28,6 +62,17 @@
         //Take that value and add it to the existing value in the savingsBalance div
 
     //Savings account withdraw funtion
+    function savingsAccountWithdrawl() {
+      var savingsBal = $('#savingsBalance').text().replace('$','');
+      var savingsNum = parseInt(savingsBal);
+      var savingsInput = $('#amountSavings').val()
+      var savingsNumTwo = parseInt(savingsInput);
+      savingsBal = savingsNum-savingsNumTwo;
+      // if (savingsBal<0) {
+      //   return alert('Your account is too low!')
+      // }
+      $('#savingsBalance').text('$'+savingsBal)
+    }
 
       //On click of the withdrawl button
 
@@ -39,5 +84,24 @@
          //Else subtract that value from the current amount in the savings account
 
 // Bonus-- get the two accounts to work with each other and allow for overdraft protection
+function overdraftProtection() {
+  var savingsBal = $('#savingsBalance').text().replace('$','');
+  var savingsNum = parseInt(savingsBal);
+  var savingsInput = $('#amountSavings').val()
+  var savingsNumTwo = parseInt(savingsInput);
+  var checkingBal = $('#checkingBalance').text().replace('$','');
+  var checkingNum = parseInt(checkingBal);
+  var checkingInput = $('#amountChecking').val()
+  var checkingNumTwo = parseInt(checkingInput);
+
+  if (checkingNum == 0 && savingsNum > 0) {
+    savingsNum = savingsNum - checkingNumTwo;
+    checkingNum = checkingNum + checkingNumTwo;
+    $('#checkingBalance').text('$'+checkingNum)
+    $('#savingsBalance').text('$'+savingsNum)
+  } else{
+    alert('YOU BROKE!!')
+  };
+};
 
 // Double Bonus-- This isnt very DRY.  Using the keyword "this" see if you can make the withdraw and deposit functions work for both accounts
